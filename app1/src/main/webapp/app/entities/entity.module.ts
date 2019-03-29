0d053f63c7205ec9b9a5a34c5fe6626f5356e717
decampsm <e.ziitron@gmail.com>
2019-03-29 08:01:17 +0100

    Initial application generated by JHipster-5.8.2

--- app1/src/main/webapp/app/entities/entity.module.ts
+++ app1/src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,54 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'product',
+        loadChildren: './product/product.module#App1ProductModule'
+      },
+      {
+        path: 'product',
+        loadChildren: './product/product.module#App1ProductModule'
+      },
+      {
+        path: 'category',
+        loadChildren: './category/category.module#App1CategoryModule'
+      },
+      {
+        path: 'product',
+        loadChildren: './product/product.module#App1ProductModule'
+      },
+      {
+        path: 'customer',
+        loadChildren: './customer/customer.module#App1CustomerModule'
+      },
+      {
+        path: 'address',
+        loadChildren: './address/address.module#App1AddressModule'
+      },
+      {
+        path: 'wish-list',
+        loadChildren: './wish-list/wish-list.module#App1WishListModule'
+      },
+      {
+        path: 'category',
+        loadChildren: './category/category.module#App1CategoryModule'
+      },
+      {
+        path: 'product',
+        loadChildren: './product/product.module#App1ProductModule'
+      },
+      {
+        path: 'customer',
+        loadChildren: './customer/customer.module#App1CustomerModule'
+      },
+      {
+        path: 'address',
+        loadChildren: './address/address.module#App1AddressModule'
+      },
+      {
+        path: 'wish-list',
+        loadChildren: './wish-list/wish-list.module#App1WishListModule'
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ],
